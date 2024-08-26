431abedde1e65c5eceb0690d3ecd182bee3e6bad
Hutecki <hubert0706@wp.pl>
2024-08-26 22:42:17 +0200

    production

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true, // Enable strict mode
+  env: {
+    API_URL: process.env.NEXT_PUBLIC_API_DOMAIN,
+  },
+  // Example of a custom configuration depending on the environment
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "http://localhost:3000" : "",
+};
 
 export default nextConfig;
